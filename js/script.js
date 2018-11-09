3ddadcbfd7323ab86449a156958c6390cee7590a
Marko Robovic <mrobovic@yahoo.com>
2018-11-09 13:24:39 +0100

    adding data modal

--- js/script.js
+++ js/script.js
@@ -1,51 +1,103 @@
+'use strict';
 const weatherData = {
-	tempUnit: 'c',
-	windSpeedUnit: 'm/s',
-	week: [
-	{ day: 'Mon', temp: 22, windDirection: 'north-east', windSpeed: 10, type: 'sunny' },
-	{ day: 'Tue', temp: 14, windDirection: 'north-west', windSpeed: 14, type: 'rainy' },
-	{ day: 'Wed', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
-	{ day: 'Thu', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'sunny' },
-	{ day: 'Fri', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
-	{ day: 'Sat', temp: 17, windDirection: 'north-west', windSpeed: 20, type: 'cloudy' },
-	{ day: 'Sun', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'rainy' }
-	]
-}
+  tempUnit: "c",
+  windSpeedUnit: "m/s",
+  week: [
+    {
+      day: "Mon",
+      temp: 22,
+      windDirection: "north-east",
+      windSpeed: 10,
+      type: "sunny"
+    },
+    {
+      day: "Tue",
+      temp: 14,
+      windDirection: "north-west",
+      windSpeed: 14,
+      type: "rainy"
+    },
+    {
+      day: "Wed",
+      temp: 17,
+      windDirection: "south-east",
+      windSpeed: 20,
+      type: "cloudy"
+    },
+    {
+      day: "Thu",
+      temp: 17,
+      windDirection: "south-east",
+      windSpeed: 20,
+      type: "sunny"
+    },
+    {
+      day: "Fri",
+      temp: 17,
+      windDirection: "south-east",
+      windSpeed: 20,
+      type: "cloudy"
+    },
+    {
+      day: "Sat",
+      temp: 17,
+      windDirection: "north-west",
+      windSpeed: 20,
+      type: "cloudy"
+    },
+    {
+      day: "Sun",
+      temp: 17,
+      windDirection: "south-east",
+      windSpeed: 20,
+      type: "rainy"
+    }
+  ]
+};
 
-let element = document.getElementById('main'),
-	dayInWeek = document.getElementsByClassName('dayInWeek'),
-  wetherIcon = document.getElementsByClassName('wetherIcon'),
-  temperature = document.getElementsByClassName('temperature'),
-  windSpeed = document.getElementsByClassName('windSpeed'),
-  windDirections = document.getElementsByClassName('windDirections')[0];
-
-
+let element = document.getElementById("main"),
+  dayInWeek = document.getElementsByClassName("dayInWeek")[0],
+  wetherIcon = document.getElementsByClassName("wetherIcon")[0],
+  temperature = document.getElementsByClassName("temperature")[0],
+  windSpeeds = document.getElementsByClassName("windSpeed")[0],
+  windDirections = document.getElementsByClassName("windDirections")[0],
+  modal = document.getElementById("modal-content"),
+  close = document.getElementsByClassName("close")[0];
 
 weatherData.week.forEach(day => {
-	let eachDay = document.createElement("li");
-	eachDay.classList.add(day.day);
-	eachDay.addEventListener('click', function () {
-		
+  let eachDay = document.createElement("li");
+  eachDay.classList.add(day.day);
 
-    dayInWeek.innerText = day.day;
-    wetherIcon.innerText = day.type;
-    temperature.innerText = day.temp;
-    windSpeed.innerText = day.windSpeed;
-    windDirections.innerText = day.windDirection;
-
-    windArrow(day.windDirection);
-    console.log(dayInWeek, wetherIcon, windDirections);
-	});
-	eachDay.innerHTML = `<span class='day'>${day.day}</span><span class='temp'>${day.temp}</span>`;
-	element.appendChild(eachDay);
+  eachDay.addEventListener("click", function() {
+  	windArrow(day.windDirection);
+  	openModal(day.day, day.type, day.temp, day.windSpeed, day.windDirection);
+  });
+  
+  eachDay.innerHTML = `<span class='day'>${day.day}</span><span class='temp'>${
+    day.temp
+  }</span>`;
+  element.appendChild(eachDay);
 });
 
-function windArrow (direction) {
-	console.log(windDirections);
-		if (direction === 'north-east') {
+function windArrow(direction) {
+  if (direction === "north-east") {
+    windDirections.classList.add("north-east");
+  } else {
+    windDirections.classList.add("south-east");
+  }
+}
 
-			windDirections.classList.add('south-east');
-		}else {
-			windDirections.classList.add('north-west');
-		}
+function openModal(day, type, temp, windSpeed, windDirection) {
+ 	dayInWeek.innerText = day;
+  wetherIcon.innerText = type;
+  temperature.innerText = temp;
+  windSpeeds.innerText = windSpeed;
+  windDirections.innerText = windDirection;
+  modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target == close) {
+        modal.style.display = "none";
+    }
 }
