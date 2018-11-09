1a15448173bb20cb142ee78101e2045abaa0496e
Marko Robovic <mrobovic@yahoo.com>
2018-11-09 11:55:40 +0100

    add wind direction

--- js/script.js
+++ js/script.js
@@ -7,34 +7,45 @@ const weatherData = {
 	{ day: 'Wed', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
 	{ day: 'Thu', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'sunny' },
 	{ day: 'Fri', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
-	{ day: 'Sat', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
+	{ day: 'Sat', temp: 17, windDirection: 'north-west', windSpeed: 20, type: 'cloudy' },
 	{ day: 'Sun', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'rainy' }
 	]
 }
 
+let element = document.getElementById('main'),
+	dayInWeek = document.getElementsByClassName('dayInWeek'),
+  wetherIcon = document.getElementsByClassName('wetherIcon'),
+  temperature = document.getElementsByClassName('temperature'),
+  windSpeed = document.getElementsByClassName('windSpeed'),
+  windDirections = document.getElementsByClassName('windDirections')[0];
 
-
-let element = document.getElementById('main');
 
 
 weatherData.week.forEach(day => {
 	let eachDay = document.createElement("li");
 	eachDay.classList.add(day.day);
 	eachDay.addEventListener('click', function () {
-		let dayInWeek = document.getElementsByClassName('dayInWeek'),
-    wetherIcon = document.getElementsByClassName('wetherIcon'),
-    temperature = document.getElementsByClassName('temperature'),
-    windSpeed = document.getElementsByClassName('windSpeed'),
-    windDirections = document.getElementsByClassName('windDirections');
+		
 
     dayInWeek.innerText = day.day;
-    wetherIcon.innerText = day.temp;
-    temperature.innerText = day.windDirection;
+    wetherIcon.innerText = day.type;
+    temperature.innerText = day.temp;
     windSpeed.innerText = day.windSpeed;
-    windDirections.innerText = day.type;
+    windDirections.innerText = day.windDirection;
 
+    windArrow(day.windDirection);
     console.log(dayInWeek, wetherIcon, windDirections);
 	});
 	eachDay.innerHTML = `<span class='day'>${day.day}</span><span class='temp'>${day.temp}</span>`;
 	element.appendChild(eachDay);
 });
+
+function windArrow (direction) {
+	console.log(windDirections);
+		if (direction === 'north-east') {
+
+			windDirections.classList.add('south-east');
+		}else {
+			windDirections.classList.add('north-west');
+		}
+}
